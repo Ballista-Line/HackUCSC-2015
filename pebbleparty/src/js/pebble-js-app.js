3528316ea150bda0e68691f9dd349b6c88fdad96
JaidenK <king.jaiden@yahoo.com>
2016-01-30 12:53:07 -0800

    Request now sends

--- pebbleparty/src/js/pebble-js-app.js
+++ pebbleparty/src/js/pebble-js-app.js
@@ -1,10 +1,10 @@
-
 //NOTE: ready is only sent once per run of pebbleparty
 Pebble.addEventListener("ready",
     function(e) {
         console.log("Hello world! - Sent from your javascript application.");
         var req = new XMLHttpRequest();
-        req.open('GET', 'http://panopticon.ballistaline.com/pebble.php?data=sdfsfsd', true);
+        req.open('GET', 'http://panopticon.ballistaline.com/pebble.php?data=From the pebble');
+        req.send();
     }
 );
 
