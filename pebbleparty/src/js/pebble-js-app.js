89531405562aff77aa6a1ccea345081c848fb832
JaidenK <king.jaiden@yahoo.com>
2016-01-30 14:13:28 -0800

    Using address

--- pebbleparty/src/js/pebble-js-app.js
+++ pebbleparty/src/js/pebble-js-app.js
@@ -1,22 +1,28 @@
 
 var token = null;
+var address = "";
 //NOTE: ready is only sent once per run of pebbleparty
 Pebble.addEventListener("ready",
     function(e) {
         token = Pebble.getWatchToken();
-        console.log("Hello world! - Sent from your javascript application.");
-        var req = new XMLHttpRequest();
-        req.open('GET', 'http://panopticon.ballistaline.com/pebble.php?create=true&id='+token);
-        req.send();
+        console.log("Hello world! - Sent from your javascript application. Token: "+token);
     }
 );
 
 Pebble.addEventListener('appmessage',
   function(e) {
-    var message = JSON.stringify(e.payload);
-    console.log('Received message: ' + message);
-    var req = new XMLHttpRequest();
-    req.open('GET', 'http://panopticon.ballistaline.com/pebble.php?data='+message+'&id='+token);
-    req.send();
+    if(e.payload.ADDRESS!=""){
+      address = e.payload.ADDRESS;
+      console.log('Using address: '+address)
+      var req = new XMLHttpRequest();
+      req.open('GET', address+'?create=true&id='+token);
+      req.send();
+    }else{
+      var message = JSON.stringify(e.payload);
+      console.log('Received message: ' + message);
+      var req = new XMLHttpRequest();
+      req.open('GET', address+'?data='+message+'&id='+token);
+      req.send();
+    }
   }
 );
