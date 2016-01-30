efbd5b4ffc6bb7e61f9d0b091aa4610a74edc788
JaidenK <king.jaiden@yahoo.com>
2016-01-30 13:02:38 -0800

    Now sends message with appmessage

--- pebbleparty/src/js/pebble-js-app.js
+++ pebbleparty/src/js/pebble-js-app.js
@@ -2,14 +2,15 @@
 Pebble.addEventListener("ready",
     function(e) {
         console.log("Hello world! - Sent from your javascript application.");
-        var req = new XMLHttpRequest();
-        req.open('GET', 'http://panopticon.ballistaline.com/pebble.php?data=From the pebble');
-        req.send();
     }
 );
 
 Pebble.addEventListener('appmessage',
   function(e) {
-    console.log('Received message: ' + JSON.stringify(e.payload));
+    var message = JSON.stringify(e.payload);
+    console.log('Received message: ' + message);
+    var req = new XMLHttpRequest();
+    req.open('GET', 'http://panopticon.ballistaline.com/pebble.php?data='+message);
+    req.send();
   }
 );
