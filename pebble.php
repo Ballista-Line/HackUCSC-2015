16c8aad495c79f0689501040683c955d1d2a4ae6
JaidenK <king.jaiden@yahoo.com>
2016-01-30 14:28:57 -0800

    Don't duplicate rows

--- pebble.php
+++ pebble.php
@@ -2,18 +2,37 @@
 require "dbinfo.php";
 
 $data = mysqli_real_escape_string($conn,$_GET["data"]);
+$id = mysqli_real_escape_string($conn,$_GET["id"]);
+$over = mysqli_real_escape_string($conn,$_GET["over"]);
+$create = mysqli_real_escape_string($conn,$_GET["create"]);
 
-if($data!=""){
-   $sql = "UPDATE pebIn SET command='{$data}'";
+if($create!=""){
+   $sql = "SELECT * FROM pebIn WHERE id='{$id}'";
    $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+   if(sizeof(mysqli_fetch_assoc($result))==0){
+      $sql = "INSERT INTO pebIn SET id='{$id}'";
+      $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+      print("User created with id {$id}.");
+   }else{
+      print("User exists with id {$id}.");
+   }
+}elseif($id!=""){
+   if($data!=""){
+      $sql = "UPDATE pebIn SET command='{$data}' WHERE id='{$id}'";
+      $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+   }
+
+   $sql = "SELECT * FROM pebIn WHERE id='{$id}'";
+   $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+   while($row = mysqli_fetch_assoc($result)) {
+      print($row["command"]);
+   }
+
+   if($over!=""){
+      $sql = "UPDATE pebIn SET command='{$over}', id='{$id}'";
+      $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+   }
 }
-
-$sql = "SELECT * FROM pebIn";
-$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
-
 mysqli_close($conn);
-
-while($row = mysqli_fetch_assoc($result)) {
-   print($row["command"]);
-}
 ?>
