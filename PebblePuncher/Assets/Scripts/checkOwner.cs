5ccb6420fc38f5c53b24c7f1701d02e3ebd1caf9
wrhight <willrhight@gmail.com>
2016-01-31 04:27:36 -0800

    The Whole Game

---
+++ PebblePuncher/Assets/Scripts/checkOwner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class checkOwner : NetworkBehaviour
+{
+    public GameObject cam;
+    // Use this for initialization
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            cam.gameObject.SetActive(false);
+        }
+    }
+}
